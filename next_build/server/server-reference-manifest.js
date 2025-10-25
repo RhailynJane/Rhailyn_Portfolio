18835b5b8846181c712021d0659305a63740b341
RhailynJane <rhailynjane.cona@sait.edu.ca>
2025-10-25 12:11:16 -0700

    remove FitMindAI_App folder

--- next_build/server/server-reference-manifest.js
+++ next_build/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kbqdWTVhfjk0aDp771WE/AV401SC7GXWkqV1Nc1BIHg=\"\n}"
